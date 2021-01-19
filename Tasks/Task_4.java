c206ae513b48f0e84de652baeb6b7449f70b804f
yurachel <yurachel5@rambler.ru>
2021-01-19 18:02:11 +0300

    Added task 4

--- Tasks/Task_4.java
+++ Tasks/Task_4.java
@@ -9,17 +9,9 @@ import java.io.*;
 public class Task_4 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[4];
-        int number = Integer.parseInt(reader.readLine());
-        for (int i = 0; i < arr.length; i++) {
-            int n = (number % 10);
-            arr[i] = n;
-            number /= 10;
 
-        }
-        for (int num :
-                arr) {
-            System.out.print(num);
-        }
+        int number = Integer.parseInt(reader.readLine());
+        System.out.println("The new number is: " + number%10 + number/10%10 + number/100%10 + number/1000%10);
+
     }
 }
