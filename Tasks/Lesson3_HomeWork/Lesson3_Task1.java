0b45ed7ee15bcd46a4581951674dc08d4f312d3c
yurachel <yurachel5@rambler.ru>
2021-01-21 13:04:12 +0300

    Added homeWork lesson3 tasks 1-7

---
+++ Tasks/Lesson3_HomeWork/Lesson3_Task1.java
@@ -0,0 +1,101 @@
+package Tasks.Lesson3_HomeWork;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* В переменную записываем число.Надо вывести на экран сколько в этом числе цифр и положительное оно,
+или отрицательное.Например,"это однозначное положительное число".Достаточно будет определить,является
+ли число однозначным,"двузначным или трехзначным и более".
+
+ */
+public class Lesson3_Task1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        if (number == 0) {
+            System.out.println("Число равно 0");
+        } else if (number == Integer.MAX_VALUE || number == Integer.MIN_VALUE) {
+            System.out.println("You have reached max or min value of Integer! Congratulation!!!");
+        } else {
+            int divided = 10;
+            int count = 1;
+            while (true) {
+                int number2 = number % divided;
+                if (number2 == number) break;
+                count++;
+                divided *= 10;
+            }
+            switch (count) {
+                case 1:
+                    if (number > 0) {
+                        System.out.println("это однозначное положительное число ");
+                    } else {
+                        System.out.println("это однозначное отрицательное число");
+                    }
+                    break;
+                case 2:
+                    if (number > 0) {
+                        System.out.println("это двузначное положительное число ");
+                    } else {
+                        System.out.println("это двузначное отрицательное число");
+                    }
+                    break;
+                case 3:
+                    if (number > 0) {
+                        System.out.println("это трехзначное положительное число ");
+                    } else {
+                        System.out.println("это трехзначное отрицательное число");
+                    }
+                    break;
+                case 4:
+                    if (number > 0) {
+                        System.out.println("это четырехзначное положительное число ");
+                    } else {
+                        System.out.println("это четырехзначное отрицательное число");
+                    }
+                    break;
+                case 5:
+                    if (number > 0) {
+                        System.out.println("это пятизначное положительное число ");
+                    } else {
+                        System.out.println("это пятизначное отрицательное число");
+                    }
+                    break;
+                case 6:
+                    if (number > 0) {
+                        System.out.println("это шестизначное положительное число ");
+                    } else {
+                        System.out.println("это шестизначное отрицательное число");
+                    }
+                    break;
+                case 7:
+                    if (number > 0) {
+                        System.out.println("это семизначное положительное число ");
+                    } else {
+                        System.out.println("это семизначное отрицательное число");
+                    }
+                    break;
+                case 8:
+                    if (number > 0) {
+                        System.out.println("это восьмизначное положительное число ");
+                    } else {
+                        System.out.println("это восьмизначное отрицательное число");
+                    }
+                    break;
+                case 9:
+                    if (number > 0) {
+                        System.out.println("это девятизначное положительное число ");
+                    } else {
+                        System.out.println("это девятизначное отрицательное число");
+                    }
+                    break;
+
+            }
+
+
+
+        }
+
+    }
+}
---
+++ Tasks/Lesson3_HomeWork/Lesson3_Task4.java
@@ -0,0 +1,28 @@
+package Tasks.Lesson3_HomeWork;
+
+//Даны 3 целых числа.Найти количество положительных чисел в исходном наборе.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Lesson3_Task4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int firstNumber = Integer.parseInt(reader.readLine());
+        int secondNumber = Integer.parseInt(reader.readLine());
+        int thirdNumber = Integer.parseInt(reader.readLine());
+
+        int count = 0;
+        if (firstNumber > 0) {
+            count++;
+        }
+        if (secondNumber > 0) {
+            count++;
+        }
+        if (thirdNumber > 0) {
+            count++;
+        }
+        System.out.println("Количество положительных чисел в наборе : " + count);
+    }
+}
