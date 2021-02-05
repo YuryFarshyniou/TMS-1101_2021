396fb1dce72f8647f6dff639f22e83f61981bff8
yurachel <yurachel5@rambler.ru>
2021-02-05 19:06:55 +0300

    Added tasks from 18 to 23.

---
+++ Tasks/Lesson_8/Methodichka_20.java
@@ -0,0 +1,26 @@
+package Tasks.Lesson_8;
+
+/*Имеется строка с текстом. Вывести текст, составленный из последних букв
+всех слов. */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Methodichka_20 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter any string: ");
+        String anyString = reader.readLine();
+        String regex = "\\w\\b";
+        StringBuilder stringBuilder = new StringBuilder();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(anyString);
+        while (matcher.find()) {
+            stringBuilder.append(matcher.group());
+        }
+        System.out.println(stringBuilder);
+    }
+}
